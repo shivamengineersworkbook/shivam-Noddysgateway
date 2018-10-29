963f9d55511353d0535b2910505bb7a95de32d1b
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-10-29 17:54:01 +0530

    added logout option

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -2,38 +2,48 @@ import {
     Component,
     OnInit
 } from '@angular/core';
+import { Router } from "@angular/router";
+import { UserLoginService } from "../../service/user-login.service";
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  cities = [
+    {
+      value: "Delhi",
+      viewValue: "Delhi"
+    },
+    {
+      value: "Mumbai",
+      viewValue: "Mumbai"
+    },
+    {
+      value: "Kolkata",
+      viewValue: "Kolkata"
+    },
+    {
+      value: "Chennai",
+      viewValue: "Chennai"
+    },
+    {
+      value: "Bangalore",
+      viewValue: "Bangalore"
+    }
+  ];
 
-    cities = [{
-            value: 'Delhi',
-            viewValue: 'Delhi'
-        },
-        {
-            value: 'Mumbai',
-            viewValue: 'Mumbai'
-        },
-        {
-            value: 'Kolkata',
-            viewValue: 'Kolkata'
-        },
-        {
-            value: 'Chennai',
-            viewValue: 'Chennai'
-        },
-        {
-            value: 'Bangalore',
-            viewValue: 'Bangalore'
-        }
-    ];
+  constructor(public router: Router, public userService: UserLoginService) {
+      this.userService.isAuthenticated(this);
+      console.log("SecureHomeComponent: constructor");
+  }
 
-    constructor() {}
+  isLoggedIn(message: string, isLoggedIn: boolean) {
+    if (isLoggedIn) {
+      this.router.navigate(["/securehome"]);
+    }
+  }
 
-    ngOnInit() {}
-
+  ngOnInit() {}
 }
